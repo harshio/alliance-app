70446f41b0644a7d3116996f63d9b2303e790b05
harshio <65052326+harshio@users.noreply.github.com>
2025-07-27 23:54:20 -0400

    making my project line up more design-wise with the principles of React by making a Button component, which will be used to replace all the numerous instances of the button div within this application

---
+++ src/Button.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {useNavigate, useLocation} from 'react-router-dom';
+import { useSocket } from './WebSocketContext';
+
+//don't wanna think too hard about this rn, but I
+//imagine this is a good skeletal design
+
+interface Props {
+    text: string; //there's always text on a button
+    variation: string; //there's usually some sort of conditional JSX modifying the class of the button
+}
+
+function Button(props: Props){
+    return(
+        <div className={`button ${props.variation}`}>props.text</div>
+    );
+}
+
+export default Button;
